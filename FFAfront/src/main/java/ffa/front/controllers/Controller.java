74e1ea71be925b50d064dfbd674dfe4392bec6a1
Nick Kouthong <35278549+nikou36@users.noreply.github.com>
2019-12-01 11:11:20 -0800

    Add files via upload

---
+++ FFAfront/src/main/java/ffa/front/controllers/Controller.java
@@ -0,0 +1,20 @@
+package ffa.front.controllers;
+
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import javax.servlet.http.HttpServletRequest;
+
+@org.springframework.stereotype.Controller
+public class Controller {
+
+    @GetMapping("/")
+    public String hello(HttpServletRequest req, Model model) {
+        return "main";
+    }
+
+
+}
---
+++ FFAfront/src/test/java/ffa/front/FrontApplicationTests.java
@@ -0,0 +1,13 @@
+package ffa.front;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FrontApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
